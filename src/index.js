13c04fb285d65b1243f1978ec48d6def3b753900
antonko <anton.m.korshunov@gmail.com>
2018-09-23 01:46:46 +0300

    Deleting comments in the Solution

--- src/index.js
+++ src/index.js
@@ -11,18 +11,13 @@ var count=0;
 
   for (n = 0; n < preferences.length; n++)
   {
-//  console.log('n ' + n);
-  lov1=preferences[n]; // m - тот, кого любит n
-//console.log('lov1 ' + lov1);
-  lov2=preferences[lov1-1]; // o тот, кого любит
-//console.log('lov2 ' + lov2);
+  lov1=preferences[n]; 
+  lov2=preferences[lov1-1]; 
   lov3=preferences[lov2-1];
-//console.log('lov3 ' + lov3);
-//console.log((lov3-1)==n && (lov2-1)==preferences.indexOf(lov3));
+
     if ((lov3-1)==n && (lov2-1)==preferences.indexOf(lov3) && lov1!=lov2!=lov3)
     	{
 			count++;
-			//console.log('count is ' + count);
 			preferences[n]=undefined;
      	};
   };
